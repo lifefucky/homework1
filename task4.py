01b250ea630ea0f434247f4a5a6e3d20b6874538
lifefucky <ip.udachin@gmail.com>
2020-07-23 15:33:28 +0300

    lesson 3 task completed

--- task4.py
+++
@@ -1,9 +0,0 @@
-"""Пользователь вводит строку из нескольких слов, разделённых пробелами.
-Вывести каждое слово с новой строки. Строки необходимо пронумеровать.
-Если в слово длинное, выводить только первые 10 букв в слове"""
-
-user_words = input("Please insert words delimited by space\n")
-user_array =user_words.split(' ')
-
-for ind, el in enumerate(user_array):
-    print(f'{ind} : {el[:10]}')
