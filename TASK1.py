13f8ef4bb1d858d0a1cd92d27d4438269203e9b0
lifefucky <ip.udachin@gmail.com>
2020-07-31 17:53:07 +0300

    clean from files of lesson5

--- TASK1.py
+++
@@ -1,9 +0,0 @@
-"""Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
-Об окончании ввода данных свидетельствует пустая строка."""
-
-with open('sample_text.txt', 'w', encoding='UTF-8') as sample_text:
-    while True:
-        user_input = input("Please enter string:\n")
-        if not len(user_input):
-            break
-        sample_text.write(f'{user_input}\n')
